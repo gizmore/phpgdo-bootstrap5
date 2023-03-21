46c395a9db849e8d26d417b8ec616ab8b3f5dece
gizmore <gizmore@wechall.net>
2023-03-21 19:20:04 +0100

    '7.0.2-r1765 Hailing Hermelin'

--- Module_Bootstrap5.php
+++ Module_Bootstrap5.php
@@ -7,66 +7,69 @@ use GDO\UI\GDT_Icon;
 
 /**
  * Bootstrap5 assets.
- * 
- * @author gizmore
+ *
  * @version 7.0.1
  * @since 6.10.4
+ * @author gizmore
  */
 final class Module_Bootstrap5 extends GDO_Module
 {
-    public int $priority = 25;
-	public string $license = "MIT";
-    
-    public function getLicenseFilenames() : array
-    {
-        return [
-        	'bower_components/bootstrap/LICENSE',
-	        'bower_components/@popperjs/LICENSE.md',
-        ];
-    }
-    
-    public function getDependencies() : array
-    {
-    	return ['Core', 'JQuery'];
-    }
-    
-    public function getFriendencies() : array
-    {
-    	return ['CSS', 'Javascript'];
-    }
-    
-    ##############
-    ### Config ###
-    ##############
-    public function getConfig() : array
-    {
-        return [
-            GDT_Checkbox::make('bs5_icons')->initial('0'),
-        ];
-    }
-    public function cfgIcons() : string { return $this->getConfigVar('bs5_icons'); }
-    
-    ##############
-    ### Assets ###
-    ##############
-    public function onModuleInit()
-    {
-        if ($this->cfgIcons())
-        {
-        	GDT_Icon::$iconProvider = [BS5Icon::class, 'iconS'];
-        }
-    }
-    
-    public function onIncludeScripts() : void
-    {
-        $min = $this->cfgMinAppend();
-        $this->addBowerJS("@popperjs/core/dist/umd/popper{$min}.js");
-        $this->addBowerCSS("bootstrap/dist/css/bootstrap{$min}.css");
-        $this->addBowerJS("bootstrap/dist/js/bootstrap{$min}.js");
-        if ($this->cfgIcons())
-        {
-            $this->addBowerCSS("bootstrap-icons/font/bootstrap-icons.css");
-        }
-    }
-    
+
+	public int $priority = 25;
+	public string $license = 'MIT';
+
+	public function getLicenseFilenames(): array
+	{
+		return [
+			'bower_components/bootstrap/LICENSE',
+			'bower_components/@popperjs/LICENSE.md',
+		];
+	}
+
+	public function getDependencies(): array
+	{
+		return ['Core', 'JQuery'];
+	}
+
+	public function getFriendencies(): array
+	{
+		return ['CSS', 'Javascript'];
+	}
+
+	##############
+	### Config ###
+	##############
+	public function getConfig(): array
+	{
+		return [
+			GDT_Checkbox::make('bs5_icons')->initial('0'),
+		];
+	}
+
+	public function onModuleInit()
+	{
+		if ($this->cfgIcons())
+		{
+			GDT_Icon::$iconProvider = [BS5Icon::class, 'iconS'];
+		}
+	}
+
+	##############
+	### Assets ###
+	##############
+
+	public function cfgIcons(): string { return $this->getConfigVar('bs5_icons'); }
+
+	public function onIncludeScripts(): void
+	{
+		$min = $this->cfgMinAppend();
+		$this->addBowerJS("@popperjs/core/dist/umd/popper{$min}.js");
+		$this->addBowerCSS("bootstrap/dist/css/bootstrap{$min}.css");
+		$this->addBowerJS("bootstrap/dist/js/bootstrap{$min}.js");
+		if ($this->cfgIcons())
+		{
+			$this->addBowerCSS('bootstrap-icons/font/bootstrap-icons.css');
+		}
+	}
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gdo6-bootstrap5
+
 Bootstrap5 assets for gdo6.
